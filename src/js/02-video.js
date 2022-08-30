9d45367d4cb000a9c88406201c2e3281401f4e55
Dima1409 <57713733+Dima1409@users.noreply.github.com>
2022-08-31 00:22:39 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+//todo onPlay ............................................................................
+const onPlay = throttle(function (e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}, 1000);
+
+player.on('timeupdate', onPlay);
+//todo setCurrentTime ...............................................................
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
